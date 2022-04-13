ffffb2c029b14144d5538abd767a8a5c0dfa7930
nicheg <nico_hegemann@web.de>
2022-04-13 09:27:37 +0200

    commit before coding challenge 2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const bookings = [];
+
+function booking(flightNum = 'invalid', numPassengers = 0, price = 'invalid') {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  bookings.push(booking);
+}
+
+booking('LH233', 250);
+console.log(bookings);
